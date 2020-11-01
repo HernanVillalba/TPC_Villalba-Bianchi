0fad0c2314cbf2edcbffa6f73d9a2cd95a59f5eb
hernanVillalba <63215609+hernanVillalba@users.noreply.github.com>
2020-10-31 23:12:52 -0300

    SubidaProyecto

---
+++ Dominio/Juego.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Dominio
+{
+    public class Juego
+    {
+        public int ID { get; set; }
+        public string Nombre { get; set; }
+        public Plataforma PlataformaJuego { get; set; }
+        public string Descripcion { get; set; }
+        public Desarrollador DesarrolladorJuego { get; set; }
+        public SqlMoney Precio{ get; set; }
+        public int Stock { get; set; }
+        public string ImagenURL { get; set; }
+
+        public Juego()
+        {
+
+        }
+
+    }
+}
---
+++ Negocio/AccesoDatos.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Negocio
+{
+    public class AccesoDatos
+    {
+        public SqlDataReader Lector { get; set; }
+        public SqlConnection Conexion { get; set; }
+        public SqlCommand Comando { get; set; }
+
+        public AccesoDatos()
+        {
+            Conexion = new SqlConnection("data source =.\\SQLEXPRESS; initial catalog =Villalba-Bianchi_DB; integrated security=sspi");
+            Comando = new SqlCommand();
+            Comando.Connection = Conexion;
+        }
+    }
+}
